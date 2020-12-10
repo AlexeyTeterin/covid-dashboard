fc942debda7626424d5e513e90cf0b1ae141214e
Alisa-Pavlova <43580809+Alisa-Pavlova@users.noreply.github.com>
2020-12-10 15:06:29 +0300

    feat: add api info into table

--- js/table.js
+++ js/table.js
@@ -1,5 +1,12 @@
-import CovidData from './CovidData';
+import { getSummary, getCountries } from './CovidData.js';
 
-const DATA = new CovidData();
-DATA.getCountries();
-DATA.getSummary();
+const divDeaths = document.querySelector('.table-deaths');
+const divRecovered = document.querySelector('.table-recovered');
+const divCases = document.querySelector('.table-cases');
+
+getSummary()
+  .then((res) => {
+    divCases.innerText = res.Global.TotalConfirmed;
+    divDeaths.innerText = res.Global.TotalDeaths;
+    divRecovered.innerText = res.Global.TotalRecovered;
+  });
