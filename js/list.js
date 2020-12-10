8e25bb5e58d1b7969cd2df125c011dcaf80321c4
AlexeyTeterin <teterin.alexey@gmail.com>
2020-12-10 18:01:54 +0500

    feat: add countries sorted by total cases

--- js/list.js
+++ js/list.js
@@ -1,5 +1,29 @@
-import DATA from './CovidData.js';
+import { getSummary } from './CovidData.js';
+import { getCountries } from './CovidData.js';
 
-console.log(DATA.summary);
+getSummary().then((data) => {
+  data.Countries.forEach((country) => {
+    const row = document.createElement('div');
+    row.classList.add('list__row');
+    const name = document.createElement('div');
+    const indicator = document.createElement('div');
+    name.textContent = country.Country;
+    indicator.textContent = country.TotalConfirmed;
+    row.dataset.indicator = country.TotalConfirmed;
 
-// const summary = Object.assign({}, DATA.summary);
+    row.append(name, indicator);
+    document.querySelector('.list').append(row);
+
+    document.querySelector('.indicator').textContent = 'Total Cases';
+  });
+
+  const rows = document.querySelectorAll('.list__row');
+  const rowsSorted = Array.from(rows).sort((a, b) => {
+    const firtNum = parseInt(a.dataset.indicator, 10);
+    const secondNum = parseInt(b.dataset.indicator, 10);
+    if (firtNum > secondNum) return -1;
+    if (firtNum < secondNum) return 1;
+    return 0;
+  });
+  rows.forEach((row) => row.style.setProperty('order', rowsSorted.indexOf(row)));
+});
