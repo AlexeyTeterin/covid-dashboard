fe3ff35ea2ca3e8a321a086d018d603d31c7dd24
Alexey Teterin <teterin.alexey@gmail.com>
2020-12-10 22:16:20 +0500

    feat: add selection of 6 options

--- js/list.js
+++ js/list.js
@@ -1,31 +1,52 @@
 import { getSummary } from './CovidData.js';
 import { getCountries } from './CovidData.js';
 
-getSummary().then((data) => {
+const loadRows = (data, option) => {
   data.Countries.forEach((country) => {
-    console.log('country :>> ', country);
     const row = document.createElement('div');
     row.classList.add('list__row');
     const name = document.createElement('div');
-    const indicator = document.createElement('div');
+    const value = document.createElement('div');
     name.textContent = country.Country;
-    indicator.textContent = country.TotalConfirmed;
-    row.dataset.indicator = country.TotalConfirmed;
+    value.textContent = country[option];
     Object.assign(row.dataset, country);
 
-    row.append(name, indicator);
+    row.append(name, value);
     document.querySelector('.list').append(row);
-
-    document.querySelector('.indicator').textContent = 'Total Cases';
   });
+};
 
+const sortRows = (option) => {
   const rows = document.querySelectorAll('.list__row');
   const rowsSorted = Array.from(rows).sort((a, b) => {
-    const firtNum = parseInt(a.dataset.indicator, 10);
-    const secondNum = parseInt(b.dataset.indicator, 10);
+    const firtNum = parseInt(a.dataset[option], 10);
+    const secondNum = parseInt(b.dataset[option], 10);
     if (firtNum > secondNum) return -1;
     if (firtNum < secondNum) return 1;
     return 0;
   });
-  rows.forEach((row) => row.style.setProperty('order', rowsSorted.indexOf(row)));
-});
+  rows.forEach((row) => {
+    row.style.setProperty('order', rowsSorted.indexOf(row));
+    const value = row.children[1];
+    value.textContent = row.dataset[option];
+  });
+};
+
+const indicator = document.querySelector('.list__indicator');
+
+getSummary()
+  .then((data) => {
+    const options = Object.keys(data.Global);
+
+    indicator.addEventListener('change', () => sortRows(indicator.value));
+    options.forEach((option) => {
+      const selectOption = document.createElement('option');
+      selectOption.textContent = option;
+      indicator.appendChild(selectOption);
+      if (option === 'TotalConfirmed') selectOption.setAttribute('selected', true);
+    });
+
+    loadRows(data, 'TotalConfirmed');
+    sortRows('TotalConfirmed');
+  })
+  .catch((e) => console.log(e.message));
