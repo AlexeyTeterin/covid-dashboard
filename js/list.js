b767daf54244458a7b28dcc60d0e4c8c9ca11375
AlexeyTeterin <teterin.alexey@gmail.com>
2020-12-11 10:41:07 +0500

    feat: add values per 100k counting feat: split option's names to words

--- js/list.js
+++ js/list.js
@@ -1,6 +1,19 @@
 import { getSummary } from './CovidData.js';
 import { getCountries } from './CovidData.js';
 
+const createOptions = (data) => Object.keys(data.Global);
+
+const calcValuesPer100k = (data, country) => {
+  const result = {};
+  Object.keys(data.Global).forEach((opt) => {
+    const population = country.Premium.CountryStats.Population;
+    let valuePer100k = ((country[opt] / population) * 100000);
+    if (Number.isNaN(valuePer100k)) valuePer100k = 0;
+    result[`${opt}Per100k`] = valuePer100k.toFixed(2);
+  });
+  return result;
+};
+
 const loadRows = (data, option) => {
   data.Countries.forEach((country) => {
     const row = document.createElement('div');
@@ -9,7 +22,8 @@ const loadRows = (data, option) => {
     const value = document.createElement('div');
     name.textContent = country.Country;
     value.textContent = country[option];
-    Object.assign(row.dataset, country);
+
+    Object.assign(row.dataset, country, calcValuesPer100k(data, country));
 
     row.append(name, value);
     document.querySelector('.list').append(row);
@@ -19,8 +33,8 @@ const loadRows = (data, option) => {
 const sortRows = (option) => {
   const rows = document.querySelectorAll('.list__row');
   const rowsSorted = Array.from(rows).sort((a, b) => {
-    const firtNum = parseInt(a.dataset[option], 10);
-    const secondNum = parseInt(b.dataset[option], 10);
+    const firtNum = parseFloat(a.dataset[option], 10);
+    const secondNum = parseFloat(b.dataset[option], 10);
     if (firtNum > secondNum) return -1;
     if (firtNum < secondNum) return 1;
     return 0;
@@ -34,19 +48,33 @@ const sortRows = (option) => {
 
 const indicator = document.querySelector('.list__indicator');
 
+const splitWords = (string) => {
+  let result = string;
+  const words = ['Confirmed', 'Deaths', 'Recovered', 'Per', '100k'];
+  words.forEach((word) => {
+    const index = result.indexOf(word);
+    if (index < 0) return;
+    result = `${result.slice(0, index)} ${result.slice(index, result.length)}`;
+  });
+  return result;
+};
+
 getSummary()
   .then((data) => {
-    const options = Object.keys(data.Global);
-
-    indicator.addEventListener('change', () => sortRows(indicator.value));
+    const options = createOptions(data);
+    options.forEach((opt) => options.push(`${opt}Per100k`));
     options.forEach((option) => {
       const selectOption = document.createElement('option');
-      selectOption.textContent = option;
+      selectOption.value = option;
+      selectOption.textContent = splitWords(option);
+
       indicator.appendChild(selectOption);
       if (option === 'TotalConfirmed') selectOption.setAttribute('selected', true);
     });
 
+    indicator.addEventListener('change', () => sortRows(indicator.value));
+
     loadRows(data, 'TotalConfirmed');
     sortRows('TotalConfirmed');
   })
-  .catch((e) => console.log(e.message));
+  .catch((e) => console.log(e));
