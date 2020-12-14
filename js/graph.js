01f05ac1a04437a2fa14b411be052125149d963f
AlexeyTeterin <teterin.alexey@gmail.com>
2020-12-14 19:11:32 +0500

    feat: add graph

--- js/graph.js
+++ js/graph.js
@@ -1,3 +1,24 @@
 import { getWorldStatsByDay } from './CovidData.js';
 
-getWorldStatsByDay().then((DailyWorldStats) => console.log(DailyWorldStats));
+const canvas = document.querySelector('#graph');
+
+getWorldStatsByDay().then((DailyWorldStats) => {
+  console.log(DailyWorldStats);
+  const graph = new Chart(canvas, {
+    type: 'line',
+    data: {
+      labels: Object.keys(DailyWorldStats.cases),
+      datasets: [{
+        label: 'Total cases',
+        data: Object.values(DailyWorldStats.cases),
+      }, {
+        label: 'Total recovered',
+        data: Object.values(DailyWorldStats.recovered),
+      }, {
+        label: 'Total deaths',
+        data: Object.values(DailyWorldStats.deaths),
+      }],
+    },
+    options: {},
+  });
+});
