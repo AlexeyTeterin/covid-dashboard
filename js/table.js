bad12620c8d242467e496d8077ed46a47b223617
Alisa-Pavlova <43580809+Alisa-Pavlova@users.noreply.github.com>
2020-12-14 15:49:28 +0300

    feat: add table

--- js/table.js
+++ js/table.js
@@ -3,10 +3,68 @@ import { getSummary, getCountries } from './CovidData.js';
 const divDeaths = document.querySelector('.table-deaths');
 const divRecovered = document.querySelector('.table-recovered');
 const divCases = document.querySelector('.table-cases');
+const buttonCount = document.querySelector('.row-title-count');
+const buttonArea = document.querySelector('.row-title-area');
+const buttonAbs = document.querySelector('.row-title-abs');
 
 getSummary()
   .then((res) => {
-    divCases.innerText = res.Global.TotalConfirmed;
-    divDeaths.innerText = res.Global.TotalDeaths;
-    divRecovered.innerText = res.Global.TotalRecovered;
+    console.log(res);
+    console.log(res.Countries[140].Premium.CountryStats.Population);
+
+    const worldPopulation = 7827000000000; // get smwhr or const?
+    // default settings
+    let population = worldPopulation;
+    let source = res.Global;
+    const stat = { world: true, total: true, absolute: true };
+    let con = source.TotalConfirmed;
+    let deat = source.TotalDeaths;
+    let rec = source.TotalRecovered;
+
+    function setStat() {
+      let k = 1;
+      if (!stat.absolute) {
+        k = population / 100000;
+      }
+      if (stat.total) {
+        con = source.TotalConfirmed;
+        deat = source.TotalDeaths;
+        rec = source.TotalRecovered;
+      } else {
+        con = source.NewConfirmed;
+        deat = source.NewDeaths;
+        rec = source.NewRecovered;
+      }
+      divCases.innerText = con / k;
+      divDeaths.innerText = deat / k;
+      divRecovered.innerText = rec / k;
+    }
+
+    buttonCount.addEventListener('click', () => { // total / last day
+      stat.total = !stat.total;
+      buttonCount.innerText = stat.total ? 'Total' : 'Last day';
+      setStat();
+    });
+
+    buttonAbs.addEventListener('click', () => { // absolute / per 100
+      stat.absolute = !stat.absolute;
+      buttonAbs.innerText = stat.absolute ? 'absolute' : 'per 100k'; // get area & stats
+      setStat();
+    });
+
+    buttonArea.addEventListener('click', () => {
+      stat.world = !stat.world;
+      if (stat.world) {
+        buttonArea.innerText = 'word';
+        population = worldPopulation;
+        source = res.Global;
+      } else {
+        const countryNumber = 140; // need to get it
+        buttonArea.innerText = res.Countries[`${countryNumber}`].Country;
+        source = res.Countries[`${countryNumber}`];
+        population = res.Countries[`${countryNumber}`].Premium.CountryStats.Population;
+      }
+      setStat();
+    });
+    setStat();
   });
