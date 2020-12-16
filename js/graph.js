6b9ffd972d5abb253b3464d096cbabf85512eaf4
AlexeyTeterin <teterin.alexey@gmail.com>
2020-12-16 14:38:54 +0500

    feat: add btn switches abs/rel values on graph

--- js/graph.js
+++ js/graph.js
@@ -1,6 +1,7 @@
 import { getWorldStatsByDay, getCountryStatsByDay } from './CovidData.js';
 
 const canvas = document.querySelector('#chart');
+const graphButton = document.querySelector('#graph__button');
 
 const newCasesByDay = (totalCasesByDay) => {
   const cases = Object.values(totalCasesByDay);
@@ -10,12 +11,32 @@ const newCasesByDay = (totalCasesByDay) => {
   });
 };
 
-const casesPer100k = (totalCasesByDay) => {
-  const cases = Object.values(totalCasesByDay);
-  return cases.map((el) => el / 78270000);
+const casesPer100k = (casesByDay, population) => casesByDay
+  .map((el) => ((el / population) * 100000).toFixed(2));
+
+const updateChartData = (chart, data, countryCode) => {
+  const { cases, recovered, deaths } = data;
+  const { datasets } = chart.data;
+  const worldPopulation = 7827000000000;
+  const selectedCountry = document.querySelector('.list__row_active');
+  const population = countryCode ? selectedCountry.dataset.population : worldPopulation;
+  const relativeValues = graphButton.classList.contains('graph__button_rel');
+  datasets[0].data = Object.values(cases);
+  datasets[1].data = Object.values(recovered);
+  datasets[2].data = Object.values(deaths);
+  datasets[3].data = newCasesByDay(cases);
+  datasets[4].data = newCasesByDay(recovered);
+  datasets[5].data = newCasesByDay(deaths);
+  if (relativeValues) {
+    datasets.forEach((el) => {
+      const dataset = el;
+      dataset.data = casesPer100k(dataset.data, population);
+    });
+  }
+  chart.update();
 };
 
-const graphCountryHandler = (graph, countryCode) => {
+const handleCountrySelection = (graph, countryCode) => {
   getCountryStatsByDay(countryCode).then((res) => {
     const chart = graph;
     const activeRow = document.querySelector('.list__row_active');
@@ -28,18 +49,22 @@ const graphCountryHandler = (graph, countryCode) => {
       });
       chart.update();
     }
-    if (res.timeline) {
-      const { cases, recovered, deaths } = res.timeline;
-      chart.data.datasets[0].data = Object.values(cases);
-      chart.data.datasets[1].data = Object.values(recovered);
-      chart.data.datasets[2].data = Object.values(deaths);
-      chart.data.datasets[3].data = newCasesByDay(cases);
-      chart.data.datasets[4].data = newCasesByDay(recovered);
-      chart.data.datasets[5].data = newCasesByDay(deaths);
-      chart.update();
-    }
+    if (res.timeline) updateChartData(chart, res.timeline, countryCode);
   });
 };
+
+const addTailToLabels = (chart, tail) => chart.data.datasets
+  .forEach((el) => {
+    const dataset = el;
+    dataset.label += tail;
+  });
+
+const removeTailFromLabels = (chart, tail) => chart.data.datasets
+  .forEach((el) => {
+    const dataset = el;
+    const tailIndex = dataset.label.indexOf(tail);
+    if (tailIndex > 0) dataset.label = dataset.label.substring(0, tailIndex);
+  });
 
 getWorldStatsByDay().then((DailyWorldStats) => {
   const chart = new Chart(canvas, {
@@ -48,57 +73,22 @@ getWorldStatsByDay().then((DailyWorldStats) => {
       labels: Object.keys(DailyWorldStats.cases),
       datasets: [{
         label: 'Total confirmed',
-        data: Object.values(DailyWorldStats.cases),
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        pointRadius: 1,
-        pointHoverRadius: 2.5,
         pointBackgroundColor: '#d96459',
       }, {
         label: 'Total recovered',
-        data: Object.values(DailyWorldStats.recovered),
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        pointRadius: 1,
-        pointHoverRadius: 2.5,
         pointBackgroundColor: '#588c7e',
       }, {
         label: 'Total deaths',
-        data: Object.values(DailyWorldStats.deaths),
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        pointRadius: 1,
-        pointHoverRadius: 2.5,
         pointBackgroundColor: 'black',
-        hidden: true,
       }, {
         label: 'New confirmed',
-        data: newCasesByDay(DailyWorldStats.cases),
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        pointRadius: 1,
-        pointHoverRadius: 2.5,
         pointBackgroundColor: '#f2ae72',
-        hidden: true,
       }, {
         label: 'New recovered',
-        data: newCasesByDay(DailyWorldStats.recovered),
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        pointRadius: 1,
-        pointHoverRadius: 2.5,
         pointBackgroundColor: '#b0cfc5',
-        hidden: true,
       }, {
         label: 'New deaths',
-        data: newCasesByDay(DailyWorldStats.deaths),
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        pointRadius: 1,
-        pointHoverRadius: 2.5,
         pointBackgroundColor: 'rgba(0, 0, 0, 0.6)',
-        hidden: true,
-      }, {
-        label: 'Total confirmed per 100k',
-        data: casesPer100k(DailyWorldStats.cases),
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        pointRadius: 1,
-        pointHoverRadius: 2.5,
-        pointBackgroundColor: '#d96459',
       }],
     },
     options: {
@@ -114,10 +104,37 @@ getWorldStatsByDay().then((DailyWorldStats) => {
       },
     },
   });
+  chart.data.datasets.forEach((el, index) => {
+    const dataset = el;
+    dataset.pointRadius = 1;
+    dataset.pointHoverRadius = 2.5;
+    dataset.backgroundColor = 'rgba(0, 0, 0, 0)';
+    if (index > 2) dataset.hidden = true;
+  });
+  updateChartData(chart, DailyWorldStats);
+  chart.update();
 
   document.querySelector('.list').addEventListener('click', (event) => {
     const target = event.target.parentElement;
     if (!target.classList.contains('list__row')) return;
-    graphCountryHandler(chart, target.dataset.CountryCode);
+    handleCountrySelection(chart, target.dataset.CountryCode);
+  });
+
+  graphButton.addEventListener('click', () => {
+    const btn = graphButton;
+    const click = new Event('click', { bubbles: true });
+    const activeRow = document.querySelector('.list__row_active');
+    const tail = ' per 100k';
+    if (btn.className === 'graph__button_abs') {
+      btn.textContent = 'Relative values';
+      btn.className = 'graph__button_rel';
+      addTailToLabels(chart, tail);
+    } else {
+      btn.textContent = 'Absolute values';
+      btn.className = 'graph__button_abs';
+      removeTailFromLabels(chart, tail);
+    }
+    if (activeRow) activeRow.firstChild.dispatchEvent(click);
+    if (!activeRow) updateChartData(chart, DailyWorldStats);
   });
 });
