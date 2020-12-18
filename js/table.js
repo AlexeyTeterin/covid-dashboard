204df631197e312f6376767af5935c9eeca2d047
AlexeyTeterin <teterin.alexey@gmail.com>
2020-12-18 14:22:25 +0500

    fix: add async to table button listeners

--- js/table.js
+++ js/table.js
@@ -48,19 +48,19 @@ getSummary()
       divRecovered.innerText = round(rec / k);
     }
 
-    buttonCount.addEventListener('click', () => { // total / last day
+    buttonCount.addEventListener('click', async () => { // total / last day
       stat.total = !stat.total;
       buttonCount.innerText = stat.total ? 'Total' : 'Last day';
-      buttonCount.classList.toggle('total', stat.total);
-      buttonCount.classList.toggle('new', !stat.total);
+      await buttonCount.classList.toggle('total', stat.total);
+      await buttonCount.classList.toggle('new', !stat.total);
       setStat();
     });
 
-    buttonAbs.addEventListener('click', () => { // absolute / per 100
+    buttonAbs.addEventListener('click', async () => { // absolute / per 100
       stat.absolute = !stat.absolute;
       buttonAbs.innerText = stat.absolute ? 'absolute' : 'per 100k';
-      buttonAbs.classList.toggle('absolute', stat.absolute);
-      buttonAbs.classList.toggle('relative', !stat.absolute);
+      await buttonAbs.classList.toggle('absolute', stat.absolute);
+      await buttonAbs.classList.toggle('relative', !stat.absolute);
       setStat();
     });
     buttonArea.addEventListener('click', async () => {
